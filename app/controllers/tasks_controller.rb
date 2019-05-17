bb1a621c41503a0404dc13afca68fe72bb86a295
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-17 13:58:04 +0200

    message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,11 +14,19 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+
+    redirect_to tasks_path
   end
 
   def create
     @task = Task.new(task_params)
     @task.save
+
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
   end
 
   def update
